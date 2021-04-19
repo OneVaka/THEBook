72b74875eccc0dd34f83d6116f89d5ee92f38f4e
fraaaaaaaaaanc <82470357+fraaaaaaaaaanc@users.noreply.github.com>
2021-04-20 00:29:25 +0300

    book
    fddfg

--- Book/BOOKcheck/Controllers/HomeController.cs
+++ Book/BOOKcheck/Controllers/HomeController.cs
@@ -35,6 +35,10 @@ namespace BOOKcheck.Controllers
         {
             return View();
         }
+        public IActionResult books()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
